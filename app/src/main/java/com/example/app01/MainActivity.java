7c786ca0038dcba46110df7402d23ade96be7cc8
Luan Ilaurino <jdesenvolvedorweb@gmail.com>
2024-11-12 10:56:36 -0300

    First Commit 12.11 10:55

---
+++ app/src/main/java/com/example/app01/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.app01;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText n1, n2, n3, n4;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        n1 = findViewById(R.id.nota1);
+        n2 = findViewById(R.id.nota2);
+        n3 = findViewById(R.id.nota3);
+        n4 = findViewById(R.id.nota4);
+        result = findViewById(R.id.resultado);
+    }
+
+    public void calcularMedia(View v){
+        String nota1 = n1.getText().toString();
+        String nota2 = n2.getText().toString();
+        String nota3 = n3.getText().toString();
+        String nota4 = n4.getText().toString();
+        if(!nota1.isEmpty() && !nota2.isEmpty() && !nota3.isEmpty() && !nota4.isEmpty()){
+
+        }
+
+    }
+}
